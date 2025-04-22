0ab9811566263385885754d734842c2eee478573
rahull0328 <rmmehta2004@gmail.com>
2025-04-22 22:20:11 +0530

    major advancements

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-//middlewares
+//middleware
 app.use(express.static('Public'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -11,8 +11,8 @@ app.use(express.urlencoded({extended: true}))
 //connecting to mongoDB
 mongoose.connect('mongodb://localhost:27017/crud')
 const conn = mongoose.connection
-conn.on('connected',()=>{
-  console.log('MongoDB Connected')
+conn.on('connected', ()=>{
+  console.log("MongoDB Connected")
 })
 
 //creating the schema
@@ -20,48 +20,17 @@ const userSchema = mongoose.Schema({
   userId: String,
   username: String,
   email: String,
-  age: Number,
+  age: Number
 })
 
 //creating the model
 const user = mongoose.model('user', userSchema, 'userInfo')
 
-//displaying the ui on server
+//rendering the ui on server
 app.get('/', (req, res)=>{
   res.sendFile(__dirname+'/Public/index.html')
 })
 
-//api for fetching user data
-app.get('/api/getUser/', (req, res)=>{
-  user.find().then((data)=>{
-    res.send(data)
-  })
-})
-
-//api for inserting the data
-app.post('/api/newUser/',(req, res)=>{
-  const {id, name, course, age} = req.body;
-  const newData = new user({id, name, course, age})
-  newData.save()
-  res.json({message: "New User Added"})
-})
-
-//api for updating the data
-app.put('/api/updateUser/:id', (req, res)=>{
-  const userId = req.params.id
-  const updateData = req.body
-  user.updateOne({id: userId}, updateData).then((data)=>{
-    res.json({message:`User with ${userId} Updated!`})
-  })
-})
-
-app.delete('/api/deleteUser/:id', (req, res)=>{
-  const userId = req.params.id
-  user.deleteOne({id: userId}).then(()=>{
-    res.json({message:`User with ${userId} deleted!`})
-  })
-})
-
 app.listen(3000, ()=>{
-  console.log('Server Started on http://127.0.0.1:3000')
+  console.log("Server running on http://127.0.0.1:3000")
 })
